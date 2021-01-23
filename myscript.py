96062c88ed724a28e87c361f851cdbfaa137f2b0
alBarras <albertborraspons@gmail.com>
2021-01-23 17:13:39 +0100

    lol

--- myscript.py
+++ myscript.py
@@ -1,25 +1,32 @@
-print('Firebase Script Started!')
-import RPi.GPIO as GPIO
+print('TeleConfettiCannon Script Started!')
 from time import sleep
-GPIO.setmode(GPIO.BCM)
 
-timeBetweenChecks = 1
+timeBetweenChecks = 3
 useLeds = True
-useServos = False
+useServos = True
 
-LEDlecture_GPIOpin = 16
-LEDconnected_GPIOpin = 20
-SERVO_GPIOpin = 21
+if useLeds or useServos:
+    import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BCM)
 
-#LED (feedback)
 if useLeds:
+    LEDlecture_GPIOpin = 16
+    LEDconnected_GPIOpin = 20
     GPIO.setup(LEDlecture_GPIOpin,GPIO.OUT,initial=GPIO.LOW)
     GPIO.setup(LEDconnected_GPIOpin,GPIO.OUT,initial=GPIO.LOW)
+
 if useServos:
-    GPIO.setup(SERVO_GPIOpin,GPIO.OUT,initial=GPIO.LOW)
+    SERVO_GPIOpin = 21
+    import pigpio
+    pi = pigpio.pi()
+    pi.set_mode(SERVO_GPIOpin, pigpio.OUTPUT)
 
 while True:
     GPIO.output(LEDlecture_GPIOpin,GPIO.HIGH)
+    pi.set_servo_pulsewidth(2,2500)
     sleep(timeBetweenChecks)
     GPIO.output(LEDlecture_GPIOpin,GPIO.LOW)
+    pi.set_servo_pulsewidth(2,600)
+    sleep(timeBetweenChecks)
+    pi.set_servo_pulsewidth(2, 1500)
     sleep(timeBetweenChecks)

04ec8484b65c76fb15f1aae2803e1e8162a8d90a
alBarras <albertborraspons@gmail.com>
2021-01-31 00:47:14 +0100

    lol

--- myscript.py
+++ myscript.py
@@ -161,7 +161,7 @@ def main():
     offlineTriggerIsOn = False
     offlineSensorIsOn = False
     sensorCounter = 0
-    sensorCounterPeak = 2
+    sensorCounterPeak = 1
     while True:
         print('\n--- NEW LECTURE ---')
         lastLecture = not lastLecture
